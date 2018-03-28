f3e128acb6853b91681627e56b717c9f21f09b5a
RDemian <rdemian@github.com>
2018-03-28 16:29:09 +0500

    Исправление - 3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initMap() {
     lat: 34.87,
     lng: -111.763
   };
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: uluru
   });
@@ -13,32 +13,32 @@ function initMap() {
   });
 }
 
-/*var searchBtn = document.querySelector('.btn-search');*/
-var searchBtn = document.getElementById('btn-search');
-var searchForm = document.querySelector('.search-form');
-var findBtn = document.querySelector('.btn-find');
-var fieldDateIn = document.getElementById('in-date');
-var fieldDateOut = document.getElementById('out-date');
-var fieldCountAdult = document.getElementById('adult-count');
-var fieldCountChildren = document.getElementById('children-count');
+/*var searchBtn = document.querySelector(".btn-search");*/
+var searchBtn = document.getElementById("btn-search");
+var searchForm = document.querySelector(".search-form");
+var findBtn = document.querySelector(".btn-find");
+var fieldDateIn = document.getElementById("in-date");
+var fieldDateOut = document.getElementById("out-date");
+var fieldCountAdult = document.getElementById("adult-count");
+var fieldCountChildren = document.getElementById("children-count");
 
 
-searchForm.classList.add('search-form-hide');
+searchForm.classList.add("search-form-hide");
 
-searchBtn.addEventListener('click', function (evt) {
+searchBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  searchForm.classList.toggle('search-form-hide');
-  if (searchForm.classList.contains('search-form-bounce')) {
-    searchForm.classList.remove('search-form-bounce');
+  searchForm.classList.toggle("search-form-hide");
+  if (searchForm.classList.contains("search-form-bounce")) {
+    searchForm.classList.remove("search-form-bounce");
   }
   fieldDate.focus();
 });
 
-searchForm.addEventListener('submit', function (evt) {
+searchForm.addEventListener("submit", function (evt) {
   if (!fieldDateIn.value || !fieldDateOut.value || !fieldCountAdult.value || !fieldCountChildren.value) {
     evt.preventDefault();
-    searchForm.classList.remove('search-form-bounce');
+    searchForm.classList.remove("search-form-bounce");
     searchForm.offsetWidth = searchForm.offsetWidth;
-    searchForm.classList.add('search-form-bounce');
+    searchForm.classList.add("search-form-bounce");
   }
 });
